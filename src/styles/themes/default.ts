6b3a518fea95a432ba902b63e47970f82788cff3
jhonatangalves <jhonatangalves96@gmail.com>
2022-11-15 17:01:32 -0300

    styles: 🎨 Creating default colors

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,17 @@
+export const defaultTheme = {
+  blue: "#3294F8",
+  green: "#4FFF50",
+
+  background: "#071422",
+  "bg-issue-card": "#112131",
+  "bg-profile-card": "#0B1B2B",
+
+  title: "#E7EDF4",
+  subtitle: "#C4D4E3",
+  text: "#AFC2D4",
+
+  span: "#7B96B2",
+  label: "#3A536B",
+  border: "#1C2F41",
+  input: "#040F1A",
+} as const;
