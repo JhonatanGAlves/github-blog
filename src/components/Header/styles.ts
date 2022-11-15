b67c32561ec9dc628efee7ab60e79b3015df1676
jhonatangalves <jhonatangalves96@gmail.com>
2022-11-15 18:27:31 -0300

    style: 🎨 Defining styles to the Header component

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,12 @@
 import styled from "styled-components";
+import bgImage from "../../assets/bg-image.png";
 
-export const HeaderContainer = styled.div``;
+export const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 1.875rem 0;
+
+  background-image: url(${bgImage});
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
