1919ff94891211f97846deff575ffd0372a52298
jhonatangalves <jhonatangalves96@gmail.com>
2022-11-15 22:46:11 -0300

    feat: 👨🏽‍💻 Implementing the structure of UserCard component

--- src/components/Cards/UserCard/UserCard.tsx
+++ src/components/Cards/UserCard/UserCard.tsx
@@ -1,5 +1,49 @@
-import { UserCardContainer } from "./styles";
+import { useContext } from "react";
+import { ArrowSquareOut, GithubLogo, Buildings, Users } from "phosphor-react";
+import {
+  InfoContainer,
+  LinkToProfile,
+  UserCardContainer,
+  UserCardContent,
+  UserCardFooter,
+  UserCardHeader,
+} from "./styles";
+import { GitHubContext } from "../../../contexts/GitHubContext";
 
 export const UserCard = (): JSX.Element => {
-  return <UserCardContainer></UserCardContainer>;
+  const { user } = useContext(GitHubContext);
+
+  return (
+    <UserCardContainer>
+      <img src={user.avatar_url} alt="Profile image" />
+      <UserCardContent>
+        <UserCardHeader>
+          <h3>{user.name}</h3>
+          <LinkToProfile href={user.html_url}>
+            <span>github</span>
+            <ArrowSquareOut />
+          </LinkToProfile>
+        </UserCardHeader>
+        <p>{user.bio}</p>
+        <UserCardFooter>
+          <InfoContainer>
+            <GithubLogo />
+            <a href={user.avatar_url}>
+              <label>{user.login}</label>
+            </a>
+          </InfoContainer>
+          {user.company && (
+            <InfoContainer>
+              <Buildings />
+              <label>{user.company}</label>
+            </InfoContainer>
+          )}
+          <InfoContainer>
+            <Users />
+            <label>{`${user.followers} followers`}</label>
+          </InfoContainer>
+        </UserCardFooter>
+      </UserCardContent>
+    </UserCardContainer>
+  );
 };
