6cd8dedd94244a378985ae94384098fc4cdb8a36
jhonatangalves <jhonatangalves96@gmail.com>
2022-11-15 23:56:28 -0300

    feat: 👨🏽‍💻 Implementing the structure of Search component

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,5 +1,17 @@
-import { SearchContainer } from "./styles";
+import { useContext } from "react";
+import { GitHubContext } from "../../contexts/GitHubContext";
+import { SearchContainer, SearchHeader } from "./styles";
 
 export const Search = (): JSX.Element => {
-  return <SearchContainer></SearchContainer>;
+  const { issues } = useContext(GitHubContext);
+
+  return (
+    <SearchContainer>
+      <SearchHeader>
+        <label htmlFor="search-input">Issues</label>
+        <span>{`${issues.length} issue${issues.length === 1 ? "" : "s"}`}</span>
+      </SearchHeader>
+      <input type="text" id="search-input" placeholder="Search issues" />
+    </SearchContainer>
+  );
 };
