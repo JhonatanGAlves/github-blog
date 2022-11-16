609b0653d8cda1d279be3d7c36ec58d8ee9ba031
jhonatangalves <jhonatangalves96@gmail.com>
2022-11-16 00:32:57 -0300

    feat: 👨🏽‍💻 Rendering the issue list with function map

--- src/components/ListIssues/ListIssues.tsx
+++ src/components/ListIssues/ListIssues.tsx
@@ -1,10 +1,27 @@
+import { useContext } from "react";
+import { GitHubContext } from "../../contexts/GitHubContext";
+import { IssueCard } from "../Cards/IssueCard/IssueCard";
 import { Search } from "../Search/Search";
-import { ListIssuesContainer } from "./styles";
+import { IssuesCardContent, ListIssuesContainer } from "./styles";
 
 export const ListIssues = (): JSX.Element => {
+  const { issues } = useContext(GitHubContext);
+
   return (
     <ListIssuesContainer>
       <Search />
+      <IssuesCardContent>
+        {issues.map((i, index) => {
+          return (
+            <IssueCard
+              key={index}
+              title={i.title}
+              created_at={i.created_at}
+              body={i.body}
+            />
+          );
+        })}
+      </IssuesCardContent>
     </ListIssuesContainer>
   );
 };
--- src/components/ListIssues/styles.ts
+++ src/components/ListIssues/styles.ts
@@ -5,3 +5,9 @@ export const ListIssuesContainer = styled.div`
   flex-direction: column;
   gap: 3rem;
 `;
+
+export const IssuesCardContent = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 26rem;
+  gap: 32px;
+`;
