ac674035fb654998b0bb464875771b82597545a3
jhonatangalves <jhonatangalves96@gmail.com>
2022-11-15 23:56:59 -0300

    style: 🎨 Defining styles to the Search component

--- src/components/Search/styles.ts
+++ src/components/Search/styles.ts
@@ -1,3 +1,36 @@
 import styled from "styled-components";
 
-export const SearchContainer = styled.div``;
+export const SearchContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 4.5rem;
+  gap: 8px;
+
+  input {
+    outline: none;
+    padding: 0.875rem 1rem;
+    border: 1px solid ${({ theme }) => theme.border};
+    border-radius: 8px;
+
+    color: ${({ theme }) => theme.text};
+    background-color: transparent;
+
+    ::placeholder {
+      color: ${({ theme }) => theme.label};
+    }
+  }
+`;
+
+export const SearchHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  label {
+    font-size: 1.125rem;
+    font-weight: 700;
+  }
+
+  span {
+    font-size: 0.875rem;
+  }
+`;
