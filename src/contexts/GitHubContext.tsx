36e98682c5cdde8b8930ddc9298b845e5c096083
jhonatangalves <jhonatangalves96@gmail.com>
2022-11-15 23:55:48 -0300

    context: 🔀 Calling the getAllIssuesThisProject API in GitHubContext component and recording in issues const

--- src/contexts/GitHubContext.tsx
+++ src/contexts/GitHubContext.tsx
@@ -1,5 +1,5 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
-import { getUserFromGitHub } from "../api/api";
+import { getAllIssuesThisProject, getUserFromGitHub } from "../api/api";
 
 type UserCardType = {
   avatar_url: string;
@@ -7,12 +7,23 @@ type UserCardType = {
   bio: string;
   login: string;
   company: string;
-  followers: string;
+  followers: number;
+  html_url: string;
+};
+
+type IssuesType = {
+  id: number;
+  title: string;
+  body: string;
+  state: string;
+  comments: number;
+  created_at: string;
   html_url: string;
 };
 
 type GitHubContextType = {
   user: UserCardType;
+  issues: IssuesType[];
 };
 
 interface GitHubProviderProps {
@@ -23,12 +34,16 @@ export const GitHubContext = createContext({} as GitHubContextType);
 
 export const GitHubProvider = ({ children }: GitHubProviderProps) => {
   const [user, setUser] = useState<UserCardType>({} as UserCardType);
+  const [issues, setIssues] = useState<IssuesType[]>([]);
 
   useEffect(() => {
     getUserFromGitHub("JhonatanGAlves").then(setUser);
+    getAllIssuesThisProject("JhonatanGAlves", "github-blog").then(setIssues);
   }, []);
 
   return (
-    <GitHubContext.Provider value={{ user }}>{children}</GitHubContext.Provider>
+    <GitHubContext.Provider value={{ user, issues }}>
+      {children}
+    </GitHubContext.Provider>
   );
 };
